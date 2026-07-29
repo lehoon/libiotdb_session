afefbc05fe59b490e01d9234a1fb666983b0dc1b
lehoon <lehoons@gmail.com>
2026-07-29 09:13:28 +0800

    提交iotdb时序库c++客户端代码 支持linux下编译 也可以window下编译

---
+++ README.md
@@ -0,0 +1,26 @@
+# iotdb时序库c++客户端库
+
+这是一个用于 Apache IoTDB 时序数据库的 C++ 客户端库。
+
+## 使用说明
+
+1. 从 GitHub 克隆代码：
+
+```bash
+git clone https://github.com/<your-org>/libiotdb_session.git
+cd libiotdb_session
+```
+
+2. 更新子模块：
+
+```bash
+git submodule update --init --recursive
+```
+
+3. 执行 build.sh 脚本编译：
+
+```bash
+bash build.sh
+```
+
+编译完成后，按项目说明使用生成的库和示例。
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# create by lehoon 2024.08.08
+# 编译iotdb时序库c++客户端
+#set -x
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+GREEN_DARK='\033[0;32m'
+GREEN_UNDERLINE='\033[4;32m'
+NC='\033[0m'
+
+function red_log() {
+	echo -ee "${RED}$1${NC}"
+}
+
+function green_log() {
+	echo -e "${GREEN}$1${NC}"
+}
+
+cd thrift
+make clean
+
+if [ ! -x ./configure ]; then
+	chmod +x ./configure
+fi
+
+./configure --with-cpp --with-java=no --with-erlang=no --with-nodejs=no --with-python=no --with-py3=no --with-as3=no --with-erlang=no --with-nodejs=no --with-lua=no --with-perl=no --with-php=no --with-php_extension=no --with-dart=no --with-ruby=no --with-haskell=no --with-go=no --with-swift=no --with-rs=no --with-cl=no --with-haxe=no --with-dotnetcore=no --with-d=no --with-csharp=no --with-qt5=no 
+make -j2
+
+#thrift c++库位置 thrift/lib/cpp/.libs
+
+cd ..
+
+if [ -d build ]; then
+	rm rf build
+fi
+
+mkdir build && cd build 
+cmake ..
+make -j2
